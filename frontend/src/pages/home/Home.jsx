422ea32c65036a2262bf49db566443c21e8606d2
Durgam02 <anujdevani42@gmail.com>
2024-09-03 23:51:14 +0530

    Final

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,16 +1,13 @@
-import React from 'react'
-import MessageContainer from '../../components/mesages/MessageContainer.jsx';
-import Sidebar from '../../components/sidebar/Sidebar.jsx';
-
+import MessageContainer from "../../components/messages/MessageContainer";
+import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
 	return (
-		<div className='flex sm:h-[450px] md:h-[600px] rounded-lg overflow-hidden
-         bg-gray-400 bg-clip-padding backdrop-filter 
-         backdrop-blur-lg bg-opacity-0'>
-			<Sidebar /> {/* this will be on leftside of chat window */}
-			<MessageContainer />  {/* this will be on rightside of chat window */}
+		<div className='flex sm:h-[450px] md:h-[600px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+			<Sidebar />
+			<MessageContainer />
 		</div>
 	);
 };
 export default Home;
+
